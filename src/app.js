6ddfc938127fc9363cc8ba0b4303004aed2f9358
intwriter <98668165+intwriter@users.noreply.github.com>
2023-12-22 22:50:52 +0100

    Update path to component

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { apptheme } from './assets/styles/themes/apptheme';
-import ExampleComponent from './components/examplecomponent';
+import ExampleComponent from 'app_components_path/logical/examplecomponent';
 
 export default function App() {
     return (
