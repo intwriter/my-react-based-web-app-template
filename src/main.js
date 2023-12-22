d9dacbe7279b5a045328747345df7d4288debc96
intwriter <98668165+intwriter@users.noreply.github.com>
2023-12-22 09:31:54 +0100

    Update main.js with initial rendering

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+document.body.innerHTML = '<div id="app"></div>';
+createRoot(document.getElementById('app')).render(<></>);
