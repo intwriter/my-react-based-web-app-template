b9d4a5701115e566e3a82f6c6062ab6ddbf6678a
intwriter <98668165+intwriter@users.noreply.github.com>
2023-12-22 09:34:15 +0100

    Create config file

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const glob = require('glob');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: "production",
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Welcome',
+        }),
+    ],
+    entry: glob.sync('./src/**/*.js'),
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            '@babel/preset-env',
+                            '@babel/preset-react'
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+    output: {
+        filename: 'main.min.js',
+        path: path.resolve(__dirname, 'public'),
+        clean: true
+    }
+};
