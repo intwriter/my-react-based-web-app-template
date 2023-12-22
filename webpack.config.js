170f5319fb25e0348bb20e0615999d39b2d0ab36
intwriter <98668165+intwriter@users.noreply.github.com>
2023-12-22 19:31:19 +0100

    Removed comments, for some extra security

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const glob = require('glob');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
     mode: "production",
@@ -28,6 +29,14 @@ module.exports = {
                     }
                 }
             }
+        ]
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                extractComments: false,
+            })
         ]
     },
     output: {
