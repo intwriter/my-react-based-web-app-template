9fcf6d6ed4c2ca596481dcceea16c7498baf77b7
intwriter <98668165+intwriter@users.noreply.github.com>
2023-12-22 17:39:53 +0100

    Added theme, activated theme, and import component

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { apptheme } from './assets/styles/themes/apptheme';
+import ExampleComponent from './components/examplecomponent';
 
 export default function App() {
     return (
-        <></>
+        <ThemeProvider theme={apptheme}>
+            <ExampleComponent></ExampleComponent>
+        </ThemeProvider>
     )
 }
