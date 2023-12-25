bfa642ba422bea41d9c9fb7a631c589e1741d561
intwriter <98668165+intwriter@users.noreply.github.com>
2023-12-25 09:41:40 +0100

    Switched color code to inherited prop value

--- src/components/styled/styledexamplebuttoncomponent/component.js
+++ src/components/styled/styledexamplebuttoncomponent/component.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const StyledExampleButtonComponent = styled.button`
-    background-color: #cccccc;
+    background-color: ${props => props.theme.buttonBackgroundColor};
     padding: 5px;
     font-weight: bold;
 `;
