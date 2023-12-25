8b306f6300dff07bad7a0465799eaef15d102528
intwriter <98668165+intwriter@users.noreply.github.com>
2023-12-25 09:47:33 +0100

    Added semicolon

--- src/components/logical/examplecomponent/hooks/examplehook.js
+++ src/components/logical/examplecomponent/hooks/examplehook.js
@@ -1,7 +1,7 @@
 export default function useExampleHook() {
     
     function onClickHandler() {
-        alert("Button was clicked")
+        alert("Button was clicked");
     }
 
     return { onClick: onClickHandler };
