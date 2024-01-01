6f8c2b1579235982e6ea9d9a3d287a485781aebd
intwriter <98668165+intwriter@users.noreply.github.com>
2024-01-01 23:04:46 +0100

    Update path

--- src/components/logical/examplecomponent/examplecomponent.js
+++ src/components/logical/examplecomponent/examplecomponent.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import StyledExampleButtonComponent from 'app_components_path/styled/styledexamplebuttoncomponent/styledexamplebuttoncomponent';
-import useExampleHook from './hooks/examplehook';
+import useExampleHook from 'app_hooks_path/examplehook';
 
 export default function ExampleComponent() {
     const { onClick } = useExampleHook();
