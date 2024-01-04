5f22d8e82134decc970f3c3eafdd23e5f1ed8f83
intwriter <98668165+intwriter@users.noreply.github.com>
2024-01-04 17:01:42 +0100

    Refactoring

--- src/components/logical/examplecomponent/examplecomponent.tsx
+++ src/components/logical/examplecomponent/examplecomponent.tsx
@@ -3,10 +3,12 @@ import React from 'react';
 import StyledExampleButtonComponent from 'app_components_path/styled/styledexamplebuttoncomponent/styledexamplebuttoncomponent';
 import useExampleHook from 'app_hooks_path/examplehook';
 
-export default function ExampleComponent() {
+const ExampleComponent = () => {
     const { onClick } = useExampleHook();
 
     return (
         <StyledExampleButtonComponent onClick={onClick}>Example button</StyledExampleButtonComponent>
     );
 }
+
+export default ExampleComponent;
