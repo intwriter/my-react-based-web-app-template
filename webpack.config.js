02268c92c0ff542151999c18ee9a55435c900b8e
intwriter <98668165+intwriter@users.noreply.github.com>
2024-01-04 15:14:54 +0100

    Updated config

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,7 @@ module.exports = {
         }),
     ],
     resolve: {
+        extensions: ['.tsx', '.ts', '.jsx', '.js'],
         alias: {
             app_components_path: path.resolve(__dirname, 'src/components/'),
             app_themes_path: path.resolve(__dirname, 'src/assets/styles/themes/'),
@@ -19,19 +20,20 @@ module.exports = {
         },
     },
     entry: {
-        index: './src/index.js'
+        index: './src/index.tsx'
     },
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: /\.(tsx|ts|jsx|js)$/,
                 exclude: /node_modules/,
                 use: {
                     loader: "babel-loader",
                     options: {
                         presets: [
                             '@babel/preset-env',
-                            '@babel/preset-react'
+                            '@babel/preset-react',
+                            '@babel/preset-typescript'
                         ]
                     }
                 }
